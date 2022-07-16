c635c2f6387e226d8e361f1abe38a88fc1f04baa
fepanbr <fepanbr@gmail.com>
2022-07-16 19:36:08 +0900

    feat: 제목 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 import Map from './components/Map';
 
 function App() {
+  
   return (
     <div className="background">
+      <h1 className='app-title'>With FootPrint</h1>
       <Map />
     </div>
   );

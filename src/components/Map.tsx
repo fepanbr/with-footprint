6de12bc2a230e7bb4b443c20edcf7aa6ffa11020
fepanbr <fepanbr@gmail.com>
2022-07-16 18:29:05 +0900

    feat: 구글 맵 api 키 숨기게 추가

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function Map() {
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627
+      lat: 37.201386194163,
+      lng: 127.11778019828
     },
-    zoom: 11
+    zoom: 18
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div className='map-container' style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyD01EUhFWd-aJOG8XB6Ch67kvNkZGUY1GY" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGE_MAP_KEY as string }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        {/* <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        /> */}
       </GoogleMapReact>
     </div>
   );
